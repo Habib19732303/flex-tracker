ed256803978f6efe1739b375d233a64d84d681ab
Habib19732303 <h_rehman73@yahoo.co.uk>
2025-07-31 08:34:23 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('flex-cache').then(cache => cache.addAll(['./', './index.html', './manifest.json']))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
